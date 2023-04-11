0ce1c11cd238e214461338faaeba6f3b9f0f6e35
Angel Valladares <arivalladares2.0@gmail.com>
2023-04-10 23:06:26 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+//encriptar texto con md5 desde el formulario de index.html
+
+function encriptar() {
+    var texto = document.getElementById("textEncript").value;
+    var encriptado = md5(texto);
+    document.getElementById("encriptado").value = encriptado;
+}
+
+//desencriptar texto con md5 desde el formulario de index.html
+
+function desencriptar() {
+    var texto = document.getElementById("texto").value;
+    var desencriptado = md5(texto);
+    document.getElementById("desencriptado").value = desencriptado;
+}
